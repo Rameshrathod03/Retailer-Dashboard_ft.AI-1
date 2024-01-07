4a038a4bf437fff15df56a3f130abdebd872d04b
abhinavreddy05 <abhinavreddy05@kgpian.iitkgp.ac.in>
2024-01-07 16:38:58 +0530

    test commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-&9^i(f+x0&^6(d#m*evxs1a*p=_-arry1q47^d!#tt(4^6aivt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
